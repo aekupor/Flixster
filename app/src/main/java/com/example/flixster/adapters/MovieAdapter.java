b89d457e55a3a270a05be8f2b4c7be078d4802e8
Ashlee Kupor <akupor@fb.com>
2020-06-24 13:17:12 -0700

    Display default placeholder graphic for each image during loading.

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -2,6 +2,7 @@ package com.example.flixster.adapters;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +11,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
 
+import java.lang.annotation.Target;
 import java.util.List;
 
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    public static final String MOVIE_PLACEHOLDER = "https://courses.codepath.org/course_files/android_university_fast_track/assets/flicks_movie_placeholder.gif";
+    public static final String BACKDROP_PLACEHOLDER = "https://courses.codepath.org/course_files/android_university_fast_track/assets/flicks_backdrop_placeholder.gif";
 
     Context context;
     List<Movie> movies;
@@ -72,14 +83,35 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
             //use glide to render images
             String imageUrl;
+            String placeholderUrl;
             //if phone is in landscape, then imageURL = backdrop image
-            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 imageUrl = movie.getBackdropPath();
+                placeholderUrl = MOVIE_PLACEHOLDER;
             } else {
                 //else imageURL = poster image
                 imageUrl = movie.getPosterPath();
+                placeholderUrl = BACKDROP_PLACEHOLDER;
             }
-            Glide.with(context).load(imageUrl).into(ivPoster);
+
+            Glide.with(context)
+                    .load(imageUrl)
+                    //have default placeholder image while loading
+                    .placeholder(Drawable.createFromPath(placeholderUrl))
+                    .error(Drawable.createFromPath(placeholderUrl))
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, com.bumptech.glide.request.target.Target<Drawable> target, boolean isFirstResource) {
+                            Log.e("TAG", "Error loading image", e);
+                            return false; //return false so the error placeholder can be placed
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, com.bumptech.glide.request.target.Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            return false;
+                        }
+                    })
+                    .into(ivPoster);
         }
     }
 }
