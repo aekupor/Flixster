95a267b128a22e88dd8274ae71c8fb4e80aaefc6
Ashlee Kupor <akupor@fb.com>
2020-06-25 12:55:20 -0700

    Add rounded corners for the images.

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -28,6 +28,8 @@ import org.parceler.Parcels;
 
 import java.util.List;
 
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
     Context context;
@@ -112,8 +114,11 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
                 placeholderUrl = R.drawable.flicks_backdrop_placeholder;
             }
 
+            int radius = 30; // corner radius, higher value = more rounded
+            int margin = 10; // crop margin, set to 0 for corners with no crop
             Glide.with(context)
                     .load(imageUrl)
+                    .transform(new RoundedCornersTransformation(radius, margin))
                     //have default placeholder image while loading
                     .placeholder(placeholderUrl)
                     .error(placeholderUrl)
