7ed5d40493fa2820ec0f8126184418910ae92d35
Ashlee Kupor <akupor@fb.com>
2020-06-26 10:06:28 -0700

    [Movie Notes] Create basic NoteAdapter backbones.

--- app/src/main/java/com/example/flixster/adapters/NoteAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/NoteAdapter.java
@@ -1,4 +1,46 @@
 package com.example.flixster.adapters;
 
-public class NoteAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
+
+    List<String> items;
+
+    public NoteAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Use layout inflator to inflate a view
+        View notesView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        //wrap it inside a View Holder and return it
+        return new ViewHolder(notesView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    //Container to provide easy access to views that represent each row of the list
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
