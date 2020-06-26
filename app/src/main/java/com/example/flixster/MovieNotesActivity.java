041d661eb7f55a9d8f029b384e73d02e7b0217e9
Ashlee Kupor <akupor@fb.com>
2020-06-26 09:49:11 -0700

    [Movie Notes] Add ViewBinding to MovieNotesActivity.

--- app/src/main/java/com/example/flixster/MovieNotesActivity.java
+++ app/src/main/java/com/example/flixster/MovieNotesActivity.java
@@ -3,12 +3,26 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.flixster.databinding.ActivityMovieDetailsBinding;
+import com.example.flixster.databinding.ActivityMovieNotesBinding;
 
 public class MovieNotesActivity extends AppCompatActivity {
+
+    EditText addItem;
+    Button saveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_notes);
+        ActivityMovieNotesBinding binding = ActivityMovieNotesBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        addItem = binding.addItem;
+        saveBtn = binding.saveBtn;
     }
 }
